16441fee7864bc1e5f8696938fe02fd5493ef1fb
Martin Mach <me@martinmach.cz>
2022-05-05 17:42:21 +0200

    Init commit

---
+++ device-service/src/main/java/cz/inventi/kpj/devices/web/rest/DeviceRestController.java
@@ -0,0 +1,9 @@
+package cz.inventi.kpj.devices.web.rest;
+
+import cz.inventi.kpj.openapi.api.DevicesApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeviceRestController implements DevicesApi {
+
+}
